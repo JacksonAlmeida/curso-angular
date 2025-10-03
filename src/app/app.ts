b1f779500724258fc6288f4e95726953df1d33e5
JacksonRodrigues <jacksonnalmeida30@gmail.com>
2025-10-02 21:26:44 -0400

    feature: first component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from "./components/first-component/first-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FirstComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
